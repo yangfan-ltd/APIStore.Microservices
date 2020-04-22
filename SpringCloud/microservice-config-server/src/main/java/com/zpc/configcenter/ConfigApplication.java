819283582c38b2078327f8287affbd466e156ef6
hyyms <huiyiyunmishu365@163.com>
2020-04-22 12:38:03 +0800

    msg

---
+++ SpringCloud/microservice-config-server/src/main/java/com/zpc/configcenter/ConfigApplication.java
@@ -0,0 +1,11 @@
+package com.zpc.configcenter;
+
+/**
+ * @author ：author
+ * @date ：Created in 2020-04-22 12:07
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class ConfigApplication {
+}
