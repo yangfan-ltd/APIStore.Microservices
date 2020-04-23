b5b3661ddbd72ae6597a1138e979b015ccfd2cb5
Ñ <6571553+yangfan-ltd@user.noreply.gitee.com>
2020-04-23 16:44:07 +0800

    m

--- SpringCloud/start jar.bat
+++ SpringCloud/start jar.bat
@@ -4,9 +4,9 @@ start java -jar -Dserver.port=8086  C:\iWork\APIStore.Microservices\SpringCloud\
 start java -jar -Dserver.port=7788  C:\iWork\APIStore.Microservices\SpringCloud\microservice-config-server\target\microsrvice-api-gateway.jar
 @echo off
 timeout /T 10 /NOBREAK
-start java -jar -Dserver.port=8100  C:\iWork\APIStore.Microservices\SpringCloud\demo\target\microservice.eureka.jar
+start java -jar -Dserver.port=8100  C:\iWork\APIStore.Microservices\SpringCloud\demo\target\microservice-eureka.jar
 @echo off
-start java -jar -Dserver.port=9100  C:\iWork\APIStore.Microservices\SpringCloud\demo\target\microservice.eureka.jar
+start java -jar -Dserver.port=9100  C:\iWork\APIStore.Microservices\SpringCloud\demo\target\microservice-eureka.jar
 @echo off
 timeout /T 10 /NOBREAK
 start java -jar -Dserver.port=8081  C:\iWork\APIStore.Microservices\SpringCloud\microservice-item\target\microservice-item.jar
