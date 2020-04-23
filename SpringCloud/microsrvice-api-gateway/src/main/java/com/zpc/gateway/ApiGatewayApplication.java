8b46f5893ad270dbf41328a68723bafb3ca67017
Ñ <6571553+yangfan-ltd@user.noreply.gitee.com>
2020-04-23 15:18:28 +0800

    m

--- SpringCloud/microsrvice-api-gateway/src/main/java/com/zpc/gateway/ApiGatewayApplication.java
+++ SpringCloud/microsrvice-api-gateway/src/main/java/com/zpc/gateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.zpc.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableZuulProxy
@@ -11,5 +12,11 @@ import org.springframework.context.annotation.ComponentScan;
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class,args);
+    }
+
+    public ZuulProperties zuulProperties(){
+        ZuulProperties properties=new ZuulProperties();
+        System.out.println("properties:"+properties);
+        return properties;
     }
 }
--- SpringCloud/start jar.bat
+++ SpringCloud/start jar.bat
@@ -1,9 +1,20 @@
 @echo off
-start java -jar -Dserver.port=8086  C:\iWork\APIStore.Microservices\SpringCloud\microsrvice-api-gateway\target\microsrvice-api-gateway-1.0-SNAPSHOT.jar
+start java -jar -Dserver.port=8086  C:\iWork\APIStore.Microservices\SpringCloud\microsrvice-api-gateway\target\microsrvice-api-gateway.jar
 @echo off
-
+start java -jar -Dserver.port=7788  C:\iWork\APIStore.Microservices\SpringCloud\microservice-config-server\target\microsrvice-api-gateway.jar
 @echo off
-
+timeout /T 10 /NOBREAK
+start java -jar -Dserver.port=8100  C:\iWork\APIStore.Microservices\SpringCloud\demo\target\microservice.eureka.jar
+@echo off
+start java -jar -Dserver.port=9100  C:\iWork\APIStore.Microservices\SpringCloud\demo\target\microservice.eureka.jar
+@echo off
+timeout /T 10 /NOBREAK
+start java -jar -Dserver.port=8081  C:\iWork\APIStore.Microservices\SpringCloud\microservice-item\target\microservice-item.jar
+@echo off
+start java -jar -Dserver.port=8091  C:\iWork\APIStore.Microservices\SpringCloud\microservice-item\target\microservice-item.jar 
+@echo off
+timeout /T 10 /NOBREAK
+start java -jar -Dserver.port=8082  C:\iWork\APIStore.Microservices\SpringCloud\microservice-order\target\microservice-order.jar
 @echo off
 
 pause 
